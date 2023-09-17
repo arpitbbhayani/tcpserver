96cb212af7755c8617127f1c0f3be501c7cecfea
Shuvojit <s15sarkar@yahoo.com>
2023-09-18 00:07:37 +0530

    removing routine tracking

--- main.go
+++ main.go
@@ -4,38 +4,18 @@ import (
 	"log"
 	"math/rand"
 	"net"
-	"sync/atomic"
 	"time"
 )
 
 var (
-	MAX_THREADS     = 5
-	sem             = make(chan int, MAX_THREADS)
-	runningRoutines uint32
+	MAX_THREADS = 5
+	sem         = make(chan int, MAX_THREADS)
 )
 
 func randomDelayBetween(min int, max int) {
 	rd := rand.Intn(max-min) + min
 	log.Printf("I may take %d seconds to process...\n", rd)
 	time.Sleep(time.Duration(rd) * time.Second)
-}
-
-func Add() {
-	atomic.AddUint32(&runningRoutines, 1)
-}
-
-func Done() {
-	atomic.AddUint32(&runningRoutines, ^uint32(0))
-}
-
-func runRoutineTracker() {
-	ticker := time.NewTicker(1 * time.Second)
-	for {
-		select {
-		case <-ticker.C:
-			log.Println("Currently running routines: ", runningRoutines)
-		}
-	}
 }
 
 func process(conn net.Conn) {
@@ -50,7 +30,6 @@ func process(conn net.Conn) {
 
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
 	conn.Close()
-	Done()
 
 	// Release
 	<-sem
@@ -62,7 +41,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	go runRoutineTracker()
 	for {
 		log.Println("ready to accept a new connection")
 		conn, err := listener.Accept()
@@ -73,6 +51,5 @@ func main() {
 		// Aquire
 		sem <- 1
 		go process(conn)
-		Add()
 	}
 }
